44e7ba746150058f12d9848d5134de7b4479d07f
Daniel Wilson <daniel@loylap.com>
2020-03-23 15:41:03 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,12 @@ set -e
 mkdir -p ~/.aws
 touch ~/.aws/credentials
 
-echo "Bucket = ${AWS_S3_BUCKET}"
-echo "File = ${FILE}"
+echo ${AWS_S3_BUCKET}
+echo ${FILE}
 echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
-            --region ${AWS_REGION} $* --debug
+aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \ --region ${AWS_REGION} $* --debug
 
 rm -rf ~/.aws
